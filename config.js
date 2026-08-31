799564fb095fde2ca5e0de4516a8d82576b7ef7e
gabymhg-stack <gabymhg@gmail.com>
2026-08-31 16:43:27 -0600

    Add files via upload

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 // Es segura de exponer aquí: las reglas RLS son las que de verdad protegen los datos.
 window.QERCIA_CONFIG = {
   SUPABASE_URL: "https://nvvmqstcagskdkgfropc.supabase.co",
-  SUPABASE_ANON_KEY: "PENDIENTE_PEGAR_ANON_KEY"
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52dm1xc3RjYWdza2RrZ2Zyb3BjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgxOTg0MTYsImV4cCI6MjEwMzc3NDQxNn0.YXDMO2AF7FaoIQpm4NIwWLdB8uaLjOR5eSPoLZo8iX8"
 };
